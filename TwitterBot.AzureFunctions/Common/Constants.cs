93aa20fce77b036b0aff1af43c7d224382322acb
Zarko Bakurski <z.bakurski@levi9.com>
2020-10-08 16:26:45 +0200

    refactor

--- TwitterBot.AzureFunctions/Common/Constants.cs
+++ TwitterBot.AzureFunctions/Common/Constants.cs
@@ -5,9 +5,9 @@
         public const string TWITTER_API_KEY = "TwitterApiKey";
         public const string TWITTER_API_SECRET = "TwitterApiSecret";
 
-        public const string COSMOS_DB_API_KEY = "AuthKey";
+        public const string COSMOS_DB_API_KEY = "DatabaseAuthKey";
         public const string COSMOS_DB_DATABASE_ID = "DatabaseId";
-        public const string COSMOS_DB_ENDPOINT_URI = "EndpointUri";
+        public const string COSMOS_DB_ENDPOINT_URI = "DatabaseEndpointUri";
 
         public const string SERVICE_BUS_CONNECTION_STRING = "TwitterBotServiceBus_ConnectionString";
         public const string SERVICE_BUS_QUEUE_NAME = "TwitterBotServiceBus_QueueName";
