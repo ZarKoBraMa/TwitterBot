97c6c2c492cf8e5a44c4b92fd39e82ef8d28e0c1
Zarko Bakurski <z.bakurski@levi9.com>
2020-11-01 19:56:20 +0100

    Add Unit Tests

---
+++ TwitterBot.Tests/GetLatestTweetsFunctionTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TwitterBot.AzureFunctions.Configurations;
+using TwitterBot.AzureFunctions.Http;
+using TwitterBot.Framework.Contracts.Data;
+using TwitterBot.Framework.Types;
+using TwitterBot.Tests.Helpers;
+using Xunit;
+
+namespace TwitterBot.Tests
+{
+    public class GetLatestTweetsFunctionTests
+    {
+        private readonly Mock<IDocumentDbRepository<User>> mockUserRepository;
+        private readonly Mock<IDocumentDbRepository<Tweet>> mockTweetRepository;
+        private readonly Mock<IOptions<AppSettingsConfiguration>> mockConfig;
+        private readonly Mock<ILogger> mockLogger;
+        private readonly string UserId;
+
+        public GetLatestTweetsFunctionTests()
+        {
+            mockUserRepository = new Mock<IDocumentDbRepository<User>>();
+            mockTweetRepository = new Mock<IDocumentDbRepository<Tweet>>();
+            mockConfig = new Mock<IOptions<AppSettingsConfiguration>>();
+            mockLogger = new Mock<ILogger>();
+            UserId = "bcb5419c-262b-449c-9942-b077dcb1fc97";
+            mockConfig.Setup(p => p.Value).Returns(new AppSettingsConfiguration
+            {
+                AppSettings = new AppSettings { TweetsFilterIntervalInDays = 1 }
+            });
+        }
+
+        [Fact]
+        public async Task GetLatestTweets_Null_User_Test()
+        {
+            // Arange
+            var users = new List<User>();
+            mockUserRepository.Setup(p => p.TopAsync(
+                It.IsAny<Expression<Func<User, bool>>>(), 
+                It.IsAny<int>())).ReturnsAsync(users.AsQueryable());
+
+            var function = new GetLatestTweets(
+                mockUserRepository.Object,
+                mockTweetRepository.Object,
+                mockConfig.Object);
+            
+            // Act
+            var result = await function.Run(HttpTestHelper.CreateHttpRequest(
+                string.Format("https://localhost/api/getlatesttweets?uid={0}", UserId), "GET"),
+                mockLogger.Object);
+            var jsonObject = (JsonResult)result;
+
+            // Assert
+            Assert.Null(jsonObject.Value);
+        }
+        [Fact]
+        public async Task GetLatestTweets_Null_Hashtags_Test()
+        {
+            // Arrange
+            var users = new List<User> { new User { UserId = UserId } };
+            mockUserRepository.Setup(p => p.TopAsync(
+                It.IsAny<Expression<Func<User, bool>>>(), 
+                It.IsAny<int>()))
+                .ReturnsAsync(users.AsQueryable());
+
+            var function = new GetLatestTweets(
+                mockUserRepository.Object,
+                mockTweetRepository.Object,
+                mockConfig.Object);
+
+            // Act
+            var result = await function.Run(
+                HttpTestHelper.CreateHttpRequest(
+                    string.Format("https://localhost/api/getlatesttweets?uid={0}", UserId), "GET"),
+                    mockLogger.Object);
+            var jsonObject = (JsonResult)result;
+
+            // Assert
+            Assert.Null(jsonObject.Value);
+        }
+    }
+}
---
+++ TwitterBot.Tests/Helpers/HttpTestHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+using System.IO;
+using System.Text;
+
+namespace TwitterBot.Tests.Helpers
+{
+    public class HttpTestHelper
+    {
+        public static HttpRequest CreateHttpRequest(
+            string url, 
+            string method, 
+            IHeaderDictionary headers = null, 
+            string body = null)
+        {
+            var uri = new Uri(url);
+            var request = new DefaultHttpContext().Request;
+            
+            var requestFeature = request.HttpContext.Features.Get<IHttpRequestFeature>();
+            requestFeature.Method = method;
+            requestFeature.Scheme = uri.Scheme;
+            requestFeature.Path = uri.GetComponents(UriComponents.KeepDelimiter | UriComponents.Path, UriFormat.Unescaped);
+            requestFeature.PathBase = string.Empty;
+            requestFeature.QueryString = uri.GetComponents(UriComponents.KeepDelimiter | UriComponents.Query, UriFormat.Unescaped);
+            headers = headers ?? new HeaderDictionary();
+            
+            if (!string.IsNullOrEmpty(uri.Host))
+            {
+                headers.Add("Host", uri.Host);
+            }
+
+            if (body != null)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(body);
+                
+                requestFeature.Body = new MemoryStream(bytes);
+                request.ContentLength = request.Body.Length;
+                
+                headers.Add("Content-Length", request.Body.Length.ToString());
+            }
+            requestFeature.Headers = headers;
+            
+            return request;
+        }
+    }
+}
